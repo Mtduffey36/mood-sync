6ca86a96b33b4a1b8819ce5a0c2634f68ef633e7
ManuelP02 <manuelpena0207@gmail.com>
2024-07-21 18:53:04 -0400

    updated server.js file

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
+// Server things
 const express = require('express');
-const sequelize = require('./config/connection');
-const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const model = require('./models');
+const session = require('express-session');
+//Sequelize var
+const sequelize = require('./config/connection');
+
+//Calling the models
+const model = require('./models/');
+
+//Routes const
 const routes = require('./controllers');
-const bcrypt = require('bcrypt');
+
 //MiddleWare
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-//Routes
-app.use(routes);
 app.use(session({
     secret: 'your_secret_key_here',
     resave: false,
     saveUninitialized: true,
     cookie: { secure: true } 
   }));
-sequelize.sync().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server has been started on port ${PORT}`);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Routes
+app.use(routes);
+
+
+
+sequelize.sync({force: true}).then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Server Running on port ${PORT}`);
     })
 });
+
+    
