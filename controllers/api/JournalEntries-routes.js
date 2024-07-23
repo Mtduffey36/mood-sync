e24683947f61a19d2a72a3917fd79efe657ce11c
gabrielsalazar225 <165944712+gabrielsalazar225@users.noreply.github.com>
2024-07-23 18:08:04 -0400

    update

--- controllers/api/JournalEntries-routes.js
+++ controllers/api/JournalEntries-routes.js
@@ -2,3 +2,19 @@ const router = require('express').Router();
 const { JournalEntries } = require('../../models');
 const bcrypt = require('bcrypt');
 
+router.get ('/',async(req,res) => {
+    try{
+        const dbMoodData 
+    }
+})
+
+router.post ('/:mood', async (req, res) => {
+    try {
+      const dbMoodData = await Mood.Create(req.body, {
+        where: {id: req.params.id},
+      });
+    } catch(err){
+      console.log(err);
+      res.status(400).json(err);
+    }
+  });
