a4626c38cfe8efcbf0fb339793c6b66638d45fdc
Mtduffey36 <michael.t.duffey@gmail.com>
2024-07-29 18:53:01 -0500

    heatmap data fixed

--- public/js/heatmap.js
+++ public/js/heatmap.js
@@ -7,17 +7,31 @@ if(typeof CalHeatMap !== undefined) {
 
 const cal = new CalHeatMap();
 
-const data = {
-    '2024-07-01': 10,
-    '2024-07-02': 20,
-    '2024-07-03': 30,
+// const data = {
+    
+// }
 
-};
+// const data = [
+//     {date: '2024-01-01', value: 1},
+//     {date: '2024-01-02', value: 2},
+//     {date: '2024-01-03', value: 3}
+    
+// ]
+
+// const data = [
+//     { year: 2024, month: 1, day: 1, value: 10 }
+// ];
+
+const data = {
+    "1719792000": 1, // 2024-07-01
+    "1719878400": 2, // 2024-07-02
+    "1719964800": 3, // 2024-07-03
+  };
 
 const startDate = new Date('2024-01-01');
 const endDate = new Date('2024-12-31');
 
-
+console.log("data", data)
 
 cal.init({
     itemSelector: '#cal-heatmap',
@@ -32,17 +46,16 @@ cal.init({
     domainGutter: 10,
     range: 12,
     subDomainTextFormat: '%d',
-    legend: [10, 20, 30, 40],
+    legend: [1, 2, 3, 4],
     legendColors: {
         min: '#fbffbe',
         max: '#305920',
-        empty: 'black',
         base: 'blue'
     },
     itemName: ['entry', 'entries'],
     subDomainTitleFormat: {
         empty: 'No entries on {date}',
-        filled: '{count} {name} on {date}'
+        filled: 'Mood Value is {count} on {date}'
     }
 
 });
